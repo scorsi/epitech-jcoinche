101d0ef8c26bbfdf3c9f36068e794886b53d06d6
scorsi <sylvain.corsini@gmail.com>
2017-10-17 19:58:22 +0200

    Card fully fonctionnal

--- src/main/java/server/game/CardFace.java
+++ src/main/java/server/game/CardFace.java
@@ -2,23 +2,48 @@ package server.game;
 
 public enum CardFace {
 
-    Seven("7"),
-    Eight("8"),
-    Nine("9"),
-    Ten("10"),
-    Jack("Jack"),
-    Queen("Queen"),
-    King("King"),
-    As("As");
+    Seven("7", 0, 0, 0, 0),
+    Eight("8", 0, 0, 0, 0),
+    Nine("9", 9, 0, 14, 0),
+    Ten("10", 5, 10, 10, 10),
+    Jack("Jack", 14, 2, 20, 2),
+    Queen("Queen", 2, 3, 3, 3),
+    King("King", 3, 4, 4, 4),
+    As("As", 7, 19, 11, 11);
+
 
     private String name;
+    private int pointAllTrump;
+    private int pointNoTrump;
+    private int pointOneTrump;
+    private int pointIsNotTrump;
 
-    CardFace(String name) {
+    CardFace(String name, int pointAllTrump, int pointNoTrump, int pointOneTrump, int pointIsNotTrump) {
         this.name = name;
+        this.pointAllTrump = pointAllTrump;
+        this.pointNoTrump = pointNoTrump;
+        this.pointOneTrump = pointOneTrump;
+        this.pointIsNotTrump = pointIsNotTrump;
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getPointAllTrump() {
+        return pointAllTrump;
+    }
+
+    public int getPointNoTrump() {
+        return pointNoTrump;
+    }
+
+    public int getPointOneTrump() {
+        return pointOneTrump;
+    }
+
+    public int getPointIsNotTrump() {
+        return pointIsNotTrump;
     }
 
 }
