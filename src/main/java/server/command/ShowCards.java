d16a27784abc56039855171f3a5a0c19b21a6166
SkyGraff <valerian.vermeulen@epitech.eu>
2017-10-19 22:51:47 +0200

    adding ShowCards cmd Client side

--- src/main/java/server/command/ShowCards.java
+++ src/main/java/server/command/ShowCards.java
@@ -2,11 +2,29 @@ package server.command;
 
 import io.netty.channel.Channel;
 import proto.Command;
+import server.game.Card;
+import server.game.Deck;
+import server.game.Player;
 
 public class ShowCards implements ILobbyCommand {
 
     public void run(Channel channel, Command.LobbyCmd cmd, CommandManager commandManager) throws Exception {
+        if (commandManager.getLobbyManager().getWaitingPlayers().containsKey(channel)) {
+            throw new Exception();
+        }
+        else {
+            Deck deck = commandManager.getLobbyManager().getLobbyByChannel(channel).getPlayer().getDeck();
 
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.append("[SERVER] List of your cards:");
+            for (Card card : deck.getCards()) {
+                stringBuilder.append("\n-> ")
+                        .append(card.getFaceName() + " of " + card.getColorName());
+            }
+
+            commandManager.sendMsg(channel, stringBuilder.toString());
+        }
     }
 
 }
