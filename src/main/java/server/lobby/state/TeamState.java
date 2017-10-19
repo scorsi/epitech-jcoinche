6ed0b4ad493e6c9dd6b071b40ef5845b327b9b22
scorsi <sylvain.corsini@gmail.com>
2017-10-19 21:47:06 +0200

    Handle Teams server-side with full team checks.

--- src/main/java/server/lobby/state/TeamState.java
+++ src/main/java/server/lobby/state/TeamState.java
@@ -1,5 +1,6 @@
 package server.lobby.state;
 
+import io.netty.channel.Channel;
 import proto.Command.LobbyCmd;
 import server.game.Player;
 import server.lobby.Lobby;
@@ -31,12 +32,48 @@ public class TeamState extends AState {
 
     @Override
     public AState getNextState() {
-        return null;
+        return new DrawState(this.getLobby());
     }
 
     @Override
-    public void handleAction(Player player, LobbyCmd cmd) {
+    public void handleAction(Channel channel, LobbyCmd cmd) {
+        Player player = this.getLobby().getPlayer(channel);
+        if (cmd.getCmdType().equals(LobbyCmd.CmdType.TEAM)) {
+            this.handleTeam(channel, player, cmd.getValue());
+        }
+    }
 
+    private boolean isTeamFull(Player playerToNotCheck, Team teamToCheck) {
+        int i = 0;
+
+        for (Player player : this.getLobby().getPlayers()) {
+            if (!player.equals(playerToNotCheck)) {
+                if (player.getTeam().equals(teamToCheck)) {
+                    ++i;
+                }
+            }
+        }
+        return i >= 2;
+    }
+
+    private void handleTeam(Channel channel, Player player, String team) {
+        if (team.toUpperCase().equals("RED")) {
+            if (!this.isTeamFull(player, Team.Red)) {
+                this.getLobby().sendMsg("[SERVER] You changed your team to Red.", channel);
+                player.setTeam(Team.Red);
+            } else {
+                this.getLobby().sendMsg("[SERVER] This team is full, choose the other.", channel);
+            }
+        } else if (team.toUpperCase().equals("BLUE")) {
+            if (!this.isTeamFull(player, Team.Blue)) {
+                this.getLobby().sendMsg("[SERVER] You changed your team to Blue.", channel);
+                player.setTeam(Team.Blue);
+            } else {
+                this.getLobby().sendMsg("[SERVER] This team is full, choose the other.", channel);
+            }
+        } else {
+            this.getLobby().sendMsg("[SERVER] Team: Invalid parameter.", channel);
+        }
     }
 
 }
