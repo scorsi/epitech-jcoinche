a4902ce65ea150d6906a42d390a1d1def452752b
scorsi <sylvain.corsini@gmail.com>
2017-10-20 22:43:44 +0200

    Add foundCard method.

--- src/main/java/server/game/Deck.java
+++ src/main/java/server/game/Deck.java
@@ -7,6 +7,17 @@ public class Deck {
 
     private List<Card> cards = new ArrayList<>();
 
+    public int foundCard(CardFace face, CardColor color) {
+        int i = 0;
+        for (Card card : this.getCards()) {
+            if (card.getFace().equals(face) && card.getColor().equals(color)) {
+                return i;
+            }
+            ++i;
+        }
+        return -1;
+    }
+
     public List<Card> getCards() {
         return this.cards;
     }
