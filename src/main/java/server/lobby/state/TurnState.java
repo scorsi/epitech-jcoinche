156a9ded7ca9f047c5db6fb0f3834bd137fca78e
scorsi <sylvain.corsini@gmail.com>
2017-10-20 22:45:41 +0200

    Handle play cmd in TurnState.

--- src/main/java/server/lobby/state/TurnState.java
+++ src/main/java/server/lobby/state/TurnState.java
@@ -2,11 +2,12 @@ package server.lobby.state;
 
 import io.netty.channel.Channel;
 import proto.Command;
-import server.game.Card;
-import server.game.Deck;
+import server.game.*;
 import server.lobby.Lobby;
 
 public class TurnState extends AState {
+
+    int playerTurn;
 
     public TurnState(Lobby lobby) {
         super("Turn", lobby);
@@ -14,6 +15,7 @@ public class TurnState extends AState {
 
     @Override
     public AState initialize() {
+        playerTurn = 0;
         this.getLobby().broadcast("The game is ready. Let's play.", null);
         return this;
     }
@@ -30,19 +32,40 @@ public class TurnState extends AState {
 
     @Override
     public void handleAction(Channel channel, Command.LobbyCmd cmd) {
-        Deck deck = this.getLobby().getPlayer(channel).getDeck();
+        if (cmd.getCmdType().equals(Command.LobbyCmd.CmdType.SHOW_CARDS)) {
+            Deck deck = this.getLobby().getPlayer(channel).getDeck();
 
-        StringBuilder stringBuilder = new StringBuilder();
+            StringBuilder stringBuilder = new StringBuilder();
 
-        stringBuilder.append("[SERVER] List of your cards:");
-        for (Card card : deck.getCards()) {
-            stringBuilder.append("\n-> ")
-                    .append(card.getFaceName())
-                    .append(" of ")
-                    .append(card.getColorName());
+            stringBuilder.append("[SERVER] List of your cards:");
+            for (Card card : deck.getCards()) {
+                stringBuilder.append("\n-> ")
+                        .append(card.getFaceName())
+                        .append(" of ")
+                        .append(card.getColorName());
+            }
+
+            this.getLobby().sendMsg(stringBuilder.toString(), channel);
+        } else if (cmd.getCmdType().equals(Command.LobbyCmd.CmdType.PLAY_CARD)) {
+            Player player = this.getLobby().getPlayer(channel);
+            Deck deck = player.getDeck();
+            int cardIndex = deck.foundCard(CardFace.from(cmd.getCard().getFace()), CardColor.from(cmd.getCard().getColor()));
+            if (cardIndex == -1) {
+                return;
+            }
+            this.getLobby().broadcast(player.getName() + " played a " +
+                    CardFace.from(cmd.getCard().getFace()).getName() + " of " +
+                    CardColor.from(cmd.getCard().getColor()).getName() + ".", null);
+            deck.getCards().remove(cardIndex);
+            this.nextPlayer();
         }
 
-        this.getLobby().sendMsg(stringBuilder.toString(), channel);
+    }
+
+    private void nextPlayer() {
+        this.playerTurn++;
+        this.playerTurn %= 4;
+        System.out.println(this.playerTurn);
     }
 
 }
