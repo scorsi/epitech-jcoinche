aadb5a7cfeb4c662d7a38ca56c8caccb6352d6ed
scorsi <sylvain.corsini@gmail.com>
2017-10-21 18:56:21 +0200

    Fix a bug when a command like "/list-players" is miss-spelled and "/leave" is executed cause of its alias "/l". And add some aliases.

--- src/main/java/client/command/Commands.java
+++ src/main/java/client/command/Commands.java
@@ -2,17 +2,17 @@ package client.command;
 
 public enum Commands {
 
-    Username(new String[]{"/username", "/name", "/u"}, Username.class),
+    Username(new String[]{"/username", "/name", "/u", "/n"}, Username.class),
     Team(new String[]{"/team", "/t"}, Team.class),
-    Contract(new String[]{"/contract"}, Contract.class),
+    Contract(new String[]{"/put-contract", "/contract"}, Contract.class),
     Create(new String[]{"/create", "/c"}, Create.class),
     Join(new String[]{"/join", "/j"}, Join.class),
-    ListPlayers(new String[]{"/list-players"}, ListPlayers.class),
-    ListChannel(new String[]{"/list-channel"}, ListChannel.class),
+    ListPlayers(new String[]{"/list-players", "/players"}, ListPlayers.class),
+    ListChannel(new String[]{"/list-channels", "/channels"}, ListChannel.class),
     Leave(new String[]{"/leave", "/l"}, Leave.class),
-    ShowCards(new String[]{"/show-cards"}, ShowCards.class),
-    ShowTable(new String[]{"/show-table"}, ShowTable.class),
-    PlayCard(new String[]{"/play"}, PlayCard.class),
+    ShowCards(new String[]{"/show-cards", "/cards"}, ShowCards.class),
+    ShowTable(new String[]{"/show-table", "/table"}, ShowTable.class),
+    PlayCard(new String[]{"/play", "/p"}, PlayCard.class),
     ShowContract(new String[]{"/show-contract"}, ShowContract.class);
 
     private String[] aliases;
@@ -27,7 +27,7 @@ public enum Commands {
     public static Class from(String text) throws Exception {
         for (Commands command : Commands.values()) {
             for (String alias : command.getAliases()) {
-                if (text.startsWith(alias + ' ')) {
+                if (text.split("\\s+")[0].equals(alias)) {
                     return command.getCommand();
                 }
             }
